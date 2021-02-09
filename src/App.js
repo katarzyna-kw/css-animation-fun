b237865da31cb1a4ba408747bfe97c367858d77e
katarzyna_w <katarzyna.koch@gmail.com>
2021-02-08 21:41:19 -0600

    Set up Amelia component and exported to App.js

--- src/App.js
+++ src/App.js
@@ -8,10 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Amelia
-        </p>
+        <Amelia />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/Amelia.js
@@ -0,0 +1,14 @@
+function Amelia() {
+    return (
+      <div className="Amelia">
+        <header className="A-header">
+          <p>
+            Amelia!
+          </p>
+        </header>
+      </div>
+    );
+  }
+  
+  export default Amelia;
+  

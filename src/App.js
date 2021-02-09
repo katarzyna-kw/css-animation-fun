85c6c2be177b77068f2800d56bde8729dc00fa88
katarzyna_w <katarzyna.koch@gmail.com>
2021-02-08 21:43:12 -0600

    Set up Karoline component and exported to App.js

--- src/App.js
+++ src/App.js
@@ -9,14 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Amelia />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Karoline />
       </header>
     </div>
   );
---
+++ src/components/Karoline.js
@@ -0,0 +1,14 @@
+function Karoline() {
+    return (
+      <div className="Karoline">
+        <header className="K-header">
+          <p>
+            Karoline!
+          </p>
+        </header>
+      </div>
+    );
+  }
+  
+  export default Karoline;
+  

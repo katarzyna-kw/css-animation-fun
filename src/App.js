16032a91b576134ab499ff5a7e5994482a9f3159
katarzyna_w <katarzyna.koch@gmail.com>
2021-02-19 15:09:25 -0600

    breakpoints for AKJ

--- src/App.js
+++ src/App.js
@@ -33,8 +33,8 @@ function App() {
     <Amelia />
     <Karoline />
     <Julia />
-    <Slider />
-    <Portfolio />
+    {/* <Slider />
+    <Portfolio /> */}
 
 
 {/* //     <React.Fragment>
--- src/components/Karoline.js
+++ src/components/Karoline.js
@@ -4,7 +4,7 @@ function Karoline() {
     return (
       <div className="Karoline">
         <h1>
-          <span><i class="fas fa-sun"></i></span> Karoline <span>:)</span></h1>
+          <span><i class="fas fa-sun"></i></span> Karoline<span>!</span></h1>
 
       </div>    
       );

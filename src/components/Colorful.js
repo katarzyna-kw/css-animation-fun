209028f89e1186d8d7b787e91b46b06ddedcd531
katarzyna_w <katarzyna.koch@gmail.com>
2021-03-01 14:21:44 -0600

    import, function, and export to Colorful.js

---
+++ src/components/Colorful.js
@@ -0,0 +1,15 @@
+import './Colorful.css'
+
+
+function Colorful() {
+    return (
+      <div className="Colorful">
+        
+        <h1>
+        <span></span> Colors<span>!</span>
+        </h1>
+      </div>
+    );
+  }
+  
+  export default Colorful;

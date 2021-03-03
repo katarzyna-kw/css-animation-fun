c66c3c01730434bf2578d89f76d52fee65a47a2f
katarzyna_w <katarzyna.koch@gmail.com>
2021-03-03 08:09:13 -0600

    Added Gradient.js

---
+++ src/components/Gradient.js
@@ -0,0 +1,14 @@
+import './Gradient.css'
+
+function Gradient() {
+    return (
+      <div className="Gradient">
+          <h1>
+            <span><i class="far fa-grin"></i></span> pretty cool<span>!</span>
+          </h1>
+      </div>
+    );
+  }
+  
+  export default Gradient;
+  

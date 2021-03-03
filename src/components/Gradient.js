fd6f088f018aee8321df43a63cfdf3631b5953ea
katarzyna_w <katarzyna.koch@gmail.com>
2021-03-03 08:14:27 -0600

    switch fontawesome to star on .Gradient

--- src/components/Gradient.js
+++ src/components/Gradient.js
@@ -4,7 +4,7 @@ function Gradient() {
     return (
       <div className="Gradient">
           <h1>
-            <span><i class="far fa-grin"></i></span> pretty cool<span>!</span>
+            <span><i class="far fa-star"></i></span> pretty cool<span>!</span>
           </h1>
       </div>
     );

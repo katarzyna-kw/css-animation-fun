a9a1d8e6703edfd7486513c0d6497e1e590f330f
katarzyna_w <katarzyna.koch@gmail.com>
2021-03-09 20:10:01 -0600

    fade react-reveal effect added to Gradient.js

--- src/components/Gradient.js
+++ src/components/Gradient.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './Gradient.css'
+import Fade from 'react-reveal/Fade'
 
 
-function Gradient() {
+class Gradient extends Component {
+  render() {
     return (
+      <div>
+        <Fade>
       <div className="Gradient">
           <h1>
             <span><i class="far fa-star"></i></span> pretty cool<span>!</span>
           </h1>
       </div>
+      </Fade>
+      </div>
     );
   }
+}
   
   export default Gradient;
   
